ae7f4c82c497837c55473307a2b6cf3c810efe48
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 18:07:13 -0500

    new styles

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ module.exports = function(fc){
   if(fc.type === 'Feature'){
     switch(fc.geometry.type){
       case 'Point':
-        fc.geometry.coordinates = flipCoordinate(fc.geometry.coordinates)
+        fc.geometry.coordinates = flipCoordinate(fc.geometry.coordinates);
         return fc;
         break
       case 'LineString':
         fc.geometry.coordinates.forEach(function(coordinates, i){
-          coordinates = flipCoordinate(coordinates)
-          fc.geometry.coordinates[i] = coordinates
-        })
+          coordinates = flipCoordinate(coordinates);
+          fc.geometry.coordinates[i] = coordinates;
+        });
         return fc;
         break
       case 'Polygon':
         fc.geometry.coordinates.forEach(function(ring, i){
           ring.forEach(function(coordinates, k){
-            coordinates = flipCoordinate(coordinates)
-            fc.geometry.coordinates[i][k] = coordinates
-          })
-        })
+            coordinates = flipCoordinate(coordinates);
+            fc.geometry.coordinates[i][k] = coordinates;
+          });
+        });
         return fc;
         break
     }
@@ -27,21 +27,21 @@ module.exports = function(fc){
     fc.features.forEach(function(feature){
       switch(feature.geometry.type){
         case 'Point':
-          feature.geometry.coordinates = flipCoordinate(feature.geometry.coordinates)
+          feature.geometry.coordinates = flipCoordinate(feature.geometry.coordinates);
           break
         case 'LineString':
           feature.geometry.coordinates.forEach(function(coordinates, i){
-            coordinates = flipCoordinate(coordinates)
-            feature.geometry.coordinates[i] = coordinates
-          })
+            coordinates = flipCoordinate(coordinates);
+            feature.geometry.coordinates[i] = coordinates;
+          });
           break
         case 'Polygon':
           feature.geometry.coordinates.forEach(function(ring, i){
             ring.forEach(function(coordinates, k){
-              coordinates = flipCoordinate(coordinates)
-              feature.geometry.coordinates[i][k] = coordinates
-            })
-          })
+              coordinates = flipCoordinate(coordinates);
+              feature.geometry.coordinates[i][k] = coordinates;
+            });
+          });
           break
       }
     })
@@ -54,5 +54,5 @@ module.exports = function(fc){
 }
 
 var flipCoordinate = function(coordinates){
-  return([coordinates[1], coordinates[0]])
+  return([coordinates[1], coordinates[0]]);
 }
